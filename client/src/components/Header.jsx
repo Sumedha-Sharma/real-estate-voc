6434e35f2c8058c1c2f9081db709a079f2e121f5
Sumedha-Sharma <78554879+Sumedha-Sharma@users.noreply.github.com>
2024-03-03 19:06:29 +0530

    Update header and secure profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const{currentUser}=useSelector(state=>state.user)
   return (
     <header className='shadow-md bg-white' >
       <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
@@ -27,8 +29,13 @@ export default function Header() {
        <Link to='/about'>
         <li className='hidden sm:inline text-teal-700 hover:text-teal-900 hover:font-semibold'> About</li>
         </Link>
-        <Link to='/sign-in'>
-        <li className=' sm:inline text-teal-700 hover:text-teal-900 hover:font-semibold'> Sign In</li>
+        <Link to='/profile'>
+        {currentUser?
+        (<img  className='rounded-full h-7 w-7 object-cover'  src={currentUser.avatar} alt='profile'/>)
+        :
+        (<li className=' sm:inline text-teal-700 hover:text-teal-900 hover:font-semibold'> Sign In</li>)}
+        
+        
         </Link>
       </ul>
       </div>
