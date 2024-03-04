e11b330c42b4a662d720ed1e2bfc98bc2daf951c
Sumedha-Sharma <78554879+Sumedha-Sharma@users.noreply.github.com>
2024-03-04 19:44:51 +0530

    create updateListing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -32,3 +32,25 @@ res.status(200).json('listing has been deleted!!!')
     }
 
 }
+
+export  const updateListing= async(req,res,next)=>{
+    const listing= await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,'Listing not found!!!'));
+
+    }
+     if(req.user.id!==listing.userRef){
+        return next(errorHandler(401,'You can only edit your own listings!!!'));
+     }
+
+     try {
+        const updatedListing=  await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+        res.status(200).json(updatedListing);
+     } catch (error) {
+        next(error);
+     }
+}
