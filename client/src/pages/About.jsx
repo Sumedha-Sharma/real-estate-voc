390ec2660c357010d6b1f8cdd5538a2348baa4d4
Sumedha-Sharma <78554879+Sumedha-Sharma@users.noreply.github.com>
2024-03-06 12:40:53 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto' >
+
+      <h1 className='text-4xl font-bold mb-4 text-teal-900'>
+            About NexVita
+      </h1>
+      <p className='mb-4 text-teal-800'>
+       NexVita redefines your real estate journey with a user-centric platform, ensuring a seamless browsing experience. Tailored to your preferences, our diverse property options open the door to your dream home or investment. Explore, connect, and discover the perfect space effortlessly at NexVita.
+       </p>
+       <p className='mb-4 text-teal-800'> At NexVita, we simplify the complex world of real estate. Our intuitive platform effortlessly guides you through a curated selection of properties, ensuring every click brings you closer to your perfect space. With NexVita, finding a home or investment that suits your unique needs is a straightforward and enjoyable process.
+       </p>
+       <p  className='mb-4 text-teal-800'>
+ NexVita stands as a beacon of innovation in the real estate landscape. By leveraging cutting-edge technology, we connect you with a myriad of tailored options. Navigate through diverse listings with ease, allowing NexVita to be your trusted partner in finding the ideal property for your lifestyle and investment goals.
+ </p>
+    </div>
   )
 }
